309a520d7eeb8888847fde4512ef3988e0e97a41
gloria-eme <glorymecas@hotmail.com>
2022-10-03 17:52:19 +0200

    AllLoops

--- javascript/Ejercicios/neoland-loops/app.js
+++ javascript/Ejercicios/neoland-loops/app.js
@@ -9,20 +9,30 @@ const word1sWithWord = (array, word) => {
 }
 console.log(word1sWithWord(products, "Camiseta"))
 
-//Iteración 
+//Iteración 2
 
 // Comprueba en cada uno de los usuarios que tenga al menos dos trimestres aprobados y añade la propiedad ***isApproved***
 //  a true o false en consecuencia. Una vez lo tengas compruébalo con un ***console.log***. 
 // ( **Mirar abajo en pistas** ).
 // Puedes usar este array para probar tu función:
 
-const alumns = [
+alumns = [
     {name: 'Pepe Viruela', T1: false, T2: false, T3: true}, 
 		{name: 'Lucia Aranda', T1: true, T2: false, T3: true},
 		{name: 'Juan Miranda', T1: false, T2: true, T3: true},
 		{name: 'Alfredo Blanco', T1: false, T2: false, T3: false},
 		{name: 'Raquel Benito', T1: true, T2: true, T3: true}
 ]
+
+alumns.forEach((alumns) => {    
+    if ((alumns.T1 === true && alumns.T2 === true) ||
+            (alumns.T1 === true && alumns.T3 === true) ||
+            (alumns.T2 === true && alumns.T3 === true)) {
+                alumns.isApproved = true
+        } else {
+            alumns.isApproved = false;   
+}console.log (alumns)
+})
 
 //Iteración 3
 
@@ -44,14 +54,48 @@ for (let key in alien) {
 }
 
 //Iteración 5
-// Usa un bucle for para recorrer todos los destinos del array y elimina los elementos que tengan el id 11 y 40.
-//  Imprime en un console log el array. Puedes usar este array:
 const placesToTravel2 = [{id: 5, name: 'Japan'}, {id: 11, name: 'Venecia'}, {id: 23, name: 'Murcia'}, {id: 40, name: 'Santander'}, {id: 44, name: 'Filipinas'}, {id: 59, name: 'Madagascar'}]
 
-for (let places of placesToTravel2) {
-    if (placesToTravel2.filter((places) => places.id ===11)) {
-        placesToTravel2.pop(places)
+const filterplaces = placesToTravel2.filter((place) => (place.id !== 11) && (place.id !==44));
+console.log(filterplaces);
+//no he sabido sacarlo con for, pero me ha salido con filter
+
+//Iteración 6
+
+const toys = [
+    {id: 5, name: 'Buzz MyYear'}, 
+    {id: 11, name: 'Action Woman'}, 
+    {id: 23, name: 'Barbie Man'}, 
+    {id: 40, name: 'El gato con Guantes'},
+    {id: 40, name: 'El gato felix'}
+    ]
+
+let toysNoCat= [];
+for (let toy of toys) {
+    if (toy.name.includes("gato")===false) {
+        toysNoCat.push(toy);
     }
-    console.log(placesToTravel2)  
-}
-//por terminar
+} console.log(toysNoCat);
+
+//Iteración 7
+
+const popularToys = [];
+const toys2 = [
+	{id: 5, name: 'Buzz MyYear', sellCount: 10}, 
+	{id: 11, name: 'Action Woman', sellCount: 24}, 
+	{id: 23, name: 'Barbie Man', sellCount: 15}, 
+	{id: 40, name: 'El gato con Guantes', sellCount: 8},
+	{id: 40, name: 'El gato felix', sellCount: 35}
+]
+
+for (let toy of toys2) {
+    if (toy.sellCount > 15) {
+        popularToys.push(toy);
+    }
+} console.log(popularToys);
+
+    
+
+
+
+
