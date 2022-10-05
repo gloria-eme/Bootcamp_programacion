51492b8baf967a5a2f081f28eb5368a87fa3128c
gloria-eme <glorymecas@hotmail.com>
2022-10-05 22:10:38 +0200

    DOM

--- Ejercicios/Javascript/neoland-dom/app.js
+++ Ejercicios/Javascript/neoland-dom/app.js
@@ -2,25 +2,58 @@
 
 const querySubtitle = document.querySelector(".showme");
 
-const querySubtitle = document.querySelector("#pillado");
+const querySubtitle2 = document.querySelector("#pillado");
 
 const allQueryDivs = document.querySelectorAll("p");
 
-const allQueryDivs = document.querySelectorAll(".pokemon");
+const allQueryDivs2 = document.querySelectorAll(".pokemon");
 
 //Iteración 2
 
-const firstDiv =document.querySelector("div");
-firstDiv.innerHTML = <div></div>
+//2.1
+const newDiv = document.createElement("div");
+document.body.appendChild(newDiv);
 
-const firstDiv =document.querySelector("div");
-firstDiv.innerHTML = 
-<div>
-    <p>Contiene una p</p>
-</div>
+//2.2
+const newDiv2 = document.createElement("div");
+newDiv.innerHTML = 
+`<div>
+    <p>Una p</p>
+</div>`;
+document.body.appendChild(newDiv2);
 
-const firstDiv =document.querySelector("div");
-firstDiv.innerHTML = 
-<div>
-    <p>Contiene una p</p>
-</div>
+//2.3 //for i
+const newDiv3 = document.createElement("div");
+const pElements = ["p1", "p2", "p3", "p4", "p5", "p6"];
+pElements.forEach((element) => {
+    newDiv3.innerHTML += `<p>párrafo ${element}</p>`;
+});
+document.body.appendChild(newDiv3);
+console.log(pElements)
+
+//2.4
+const newP = document.createElement("p");
+newP.innerHTML = "Soy dinámico!";
+document.body.appendChild(newP);
+
+//2.5
+const h2 = document.querySelector(".fn-insert-here");
+h2.innerHTML = 'Wubba Lubba dub dub';
+
+//2.6
+const apps = ['Facebook', 'Netflix', 'Instagram', 'Snapchat', 'Twitter'];
+const liApps = document.createElement("ul");
+apps.forEach((element) => {
+   liApps.innerHTML += `<li>${element}</li>`;
+})
+document.body.appendChild(liApps);
+
+//2.7
+const pes = document.querySelectorAll(".fn-remove-me");
+const body = document.querySelector("body")
+    body.removeChild(pes)
+
+
+//2.8 Inserta una p con el texto 'Voy en medio!' entre los dos div. 
+	//Recuerda que no solo puedes insertar elementos con .appendChild.
+const divs = document.querySelectorAll(".fn-insert-here")
--- Ejercicios/Javascript/neoland-review-basics/app.js
+++ Ejercicios/Javascript/neoland-review-basics/app.js
@@ -8,11 +8,14 @@ const movies = [
     {title: 'Madaraspar', duration: 192, categories: ['comedia', 'aventura']},
     {title: 'Spiderpan', duration: 122, categories: ['aventura', 'acción']},
     {title: 'Solo en Whatsapp', duration: 223, categories: ['comedia', 'thriller']},
-    {title: 'El gato con guantes', duration: 111, categories: ['comedia', 'aventura', 'animación']},
+    {title: 'El gato con guantes', duration: 111, categories: ['comedia', 'aventura', 'animación']}]
                     
+const valor = []
+for (let values of movies) {
+    valor = valor.push(movies.categories)}
 
 
-
+console.log(valor)
 // const arrayfMovies = (object.entries(movies))
 //  console.log(object.entries(movies))
 
